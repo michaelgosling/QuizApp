1621fd8cb0f2dbb1eb530b691c277776c23996ae
Michael Gosling <w0246627@nscc.ca>
2018-10-19 17:52:11 -0300

    Using button instead of FAB. Can't get OnClickListener to work yet.

--- app/src/main/java/me/mgosling/quizapp/MainActivity.java
+++ app/src/main/java/me/mgosling/quizapp/MainActivity.java
@@ -1,10 +1,10 @@
 package me.mgosling.quizapp;
 
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,21 +15,46 @@ public class MainActivity extends AppCompatActivity {
 
     // declare the widgets
     EditText nameEditText;
-    FloatingActionButton submitFAB;
+    Button submitBtn;
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question);
+        setContentView(R.layout.activity_main);
 
         // attach actual widgets to the variables we made
-        submitFAB = findViewById(R.id.nameSubmitFAB);
+        submitBtn = findViewById(R.id.nameSubmitBtn);
         nameEditText = findViewById(R.id.nameEditText);
 
         // set the fabs onclick listener so it checks to see if there's anything entered for
         // a name. If there is, it calls start quiz. If there's not, it shows a toast explaining
         // to the user that they need to enter their name.
-        submitFAB.setOnClickListener(new View.OnClickListener() {
+        submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (nameEditText.getText().toString().length() > 0)
